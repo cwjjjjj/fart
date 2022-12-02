0b9891d100d731962680c1fdb168bb9e8a378f79
cwjjjjj <93369602+cwjjjjj@users.noreply.github.com>
2022-12-02 23:17:14 +0800

    feat: 🎸 init

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import { Component, PropsWithChildren } from 'react'
+import './app.css'
+
+class App extends Component<PropsWithChildren> {
+
+  componentDidMount () {}
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  render () {
+    // this.props.children 是将要会渲染的页面
+    return this.props.children
+  }
+}
+
+export default App
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,24 @@
+import { Component, PropsWithChildren } from 'react'
+import { View, Text } from '@tarojs/components'
+import './index.css'
+
+export default class Index extends Component<PropsWithChildren> {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>Hello world!</Text>
+      </View>
+    )
+  }
+}
