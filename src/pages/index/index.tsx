4f3cf50783ae72f4078da04d81ef6dc192ee81ae
cwjjjjj <93369602+cwjjjjj@users.noreply.github.com>
2022-12-04 20:05:50 +0800

    feat: 🎸 init linaria

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,19 @@
 import { Button as BasicButton, View } from "@tarojs/components";
 import { AtButton } from "taro-ui";
 import "taro-ui/dist/style/components/button.scss";
+import { css, cx } from "@linaria/core";
+
+const test = css`
+  color: red;
+`;
 
 export default function index() {
   return (
-    <View>
+    <View
+      className={css`
+        color: pink;
+      `}
+    >
       index
       <BasicButton>basic button</BasicButton>
       <AtButton>按钮文案</AtButton>
